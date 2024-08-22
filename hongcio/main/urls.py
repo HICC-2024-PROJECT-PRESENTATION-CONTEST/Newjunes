634347f3899514b1137d3fb2ca94186c1305442e
skybro2004 <skybro2004@gmail.com>
2024-08-22 12:15:05 +0900

    신준: 테스트용 페이지 추가

--- hongcio/main/urls.py
+++ hongcio/main/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('test', views.test),
     path('', views.index, name='index'),
     
 ]
--- hongcio/main/views.py
+++ hongcio/main/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def test(request):
+    return render(request, 'main/index_rework.html')
+
+
 def index(request):
     return render(request, 'main/index.html')
